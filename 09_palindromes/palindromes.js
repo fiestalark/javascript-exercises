cb248273ed0ba633ffca2a28b576cee6622cdd8e
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-11-21 12:01:22 -0500

    09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz123456789';
 
+    const cleanedString = string
+                            .toLowerCase()
+                            .split('')
+                            .filter(char => alphanumerical.includes(char))
+                            .join('');
+
+    const reverseString = cleanedString.split('').reverse().join('');
+    
+    return cleanedString === reverseString;
 };
 
 // Do not edit below this line
